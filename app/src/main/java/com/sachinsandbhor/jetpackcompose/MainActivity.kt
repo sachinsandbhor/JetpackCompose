8c8f6a533c544c846dfd5c7d50c910947bf87a07
sachinsandbhor <sachinsandbhor@gmail.com>
2022-07-12 19:13:43 +0530

    - Added Row layout and it's properties.

--- app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
@@ -4,23 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ColoredBox()
+            //ColoredBox()
+            Numbers()
         }
     }
 }
@@ -42,8 +43,30 @@ fun ColoredBox() {
     )
 }
 
+@Composable
+fun Numbers() {
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.Top,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(text = "1", fontSize = 12.sp)
+        Text(text = "2", fontSize = 12.sp)
+        Text(text = "3", fontSize = 12.sp)
+        Text(text = "4", fontSize = 12.sp)
+        Text(text = "5", fontSize = 12.sp)
+        Text(text = "6", fontSize = 12.sp)
+        Text(text = "7", fontSize = 12.sp)
+        Text(text = "8", fontSize = 12.sp)
+        Text(text = "9", fontSize = 12.sp)
+        Text(text = "10", fontSize = 12.sp)
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ColoredBox()
+    //ColoredBox()
+    Numbers()
 }
