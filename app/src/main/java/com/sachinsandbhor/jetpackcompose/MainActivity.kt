9fb3ec2afa0488a7abba9807bc87f4edda0bc377
sachinsandbhor <sachinsandbhor@gmail.com>
2022-07-12 18:21:51 +0530

    - Added properties to text.

--- app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
@@ -5,7 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.sachinsandbhor.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +26,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Text(text = "Hello World")
+    Text(
+        text = stringResource(id = R.string.hello_world),
+        fontSize = 24.sp,
+        fontStyle = FontStyle.Italic,
+        color = Color.Blue,
+        fontWeight = FontWeight.ExtraBold,
+        textAlign = TextAlign.Center
+    )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.PIXEL_2
+)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
