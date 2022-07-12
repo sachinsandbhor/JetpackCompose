ac53b2bb20db2134ac496cad5dca579a0e8f5910
sachinsandbhor <sachinsandbhor@gmail.com>
2022-07-12 19:02:59 +0530

    - Added modifier to box layout.

--- app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
@@ -3,28 +3,24 @@ package com.sachinsandbhor.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.sachinsandbhor.jetpackcompose.ui.theme.JetpackComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ColoredBox()
         }
     }
 }
@@ -34,10 +30,20 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun ColoredBox() {
+    Box(
+        modifier = Modifier
+            .size(120.dp)
+            .background(Color.DarkGray)
+            .padding(16.dp)
+            .clip(RoundedCornerShape(size = 20.dp))
+            .background(Color.Green)
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
-    }
+    ColoredBox()
 }
