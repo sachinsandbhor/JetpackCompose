7aa67d6e3136ffdce3def28163fff339c6fe6752
sachinsandbhor <sachinsandbhor@gmail.com>
2022-07-12 17:17:58 +0530

    - Hello world displayed

--- app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/sachinsandbhor/jetpackcompose/MainActivity.kt
@@ -3,12 +3,8 @@ package com.sachinsandbhor.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sachinsandbhor.jetpackcompose.ui.theme.JetpackComposeTheme
 
@@ -16,28 +12,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Text(text = "Hello World")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
